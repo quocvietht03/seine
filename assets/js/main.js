562f0e5e2bce21737c3c85a69ac5dbacb84e51c0
DungDN <ducdung196qtr@gmail.com>
2024-07-13 09:15:08 +0700

    fix form newsletter

--- assets/js/main.js
+++ assets/js/main.js
@@ -231,23 +231,21 @@
 				'</svg></span>';
 			$('.woocommerce-Tabs-panel--description ul li').append(ulIcon);
 		}
-
+		
 	}
-
-	function SeineInforProduct() {
-		const $itemInfo = $('#bt-product-additional_information .woocommerce-product-attributes-item__label')
-		if (!$itemInfo.length) return;
-
-		const $infoIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 26 26" fill="none"><g clip-path="url(#clip0_72_1636)"><path fill-rule="evenodd" clip-rule="evenodd" d="M14.8948 0.536215C14.3234 0.190003 13.6681 0.00695801 13 0.00695801C12.3319 0.00695801 11.6766 0.190003 11.1053 0.536215L2.574 5.70696C2.03639 6.03272 1.59185 6.49158 1.28329 7.03924C0.974733 7.5869 0.812585 8.20486 0.8125 8.83346V17.1665C0.812585 17.7951 0.974733 18.413 1.28329 18.9607C1.59185 19.5084 2.03639 19.9672 2.574 20.293L11.1053 25.4637C11.6766 25.8099 12.3319 25.993 13 25.993C13.6681 25.993 14.3234 25.8099 14.8948 25.4637L23.426 20.293C23.9636 19.9672 24.4082 19.5084 24.7167 18.9607C25.0253 18.413 25.1874 17.7951 25.1875 17.1665V8.83346C25.1874 8.20486 25.0253 7.5869 24.7167 7.03924C24.4082 6.49158 23.9636 6.03272 23.426 5.70696L14.8948 0.536215ZM16.6075 9.29496C16.7191 9.17522 16.8536 9.07918 17.0031 9.01257C17.1526 8.94596 17.314 8.91014 17.4777 8.90725C17.6413 8.90437 17.8038 8.93447 17.9556 8.99577C18.1074 9.05706 18.2452 9.1483 18.3609 9.26403C18.4767 9.37976 18.5679 9.51761 18.6292 9.66937C18.6905 9.82112 18.7206 9.98367 18.7177 10.1473C18.7148 10.311 18.679 10.4723 18.6124 10.6218C18.5458 10.7713 18.4497 10.9059 18.33 11.0175L11.83 17.5175C11.6015 17.7457 11.2917 17.8739 10.9688 17.8739C10.6458 17.8739 10.336 17.7457 10.1075 17.5175L6.8575 14.2675C6.64222 14.0364 6.52502 13.7309 6.53059 13.4151C6.53616 13.0994 6.66407 12.7981 6.88736 12.5748C7.11066 12.3515 7.41191 12.2236 7.72765 12.2181C8.04339 12.2125 8.34897 12.3297 8.58 12.545L10.9688 14.9337L16.6075 9.29496Z" fill="#1FBECD"/></g><defs><clipPath id="clip0_72_1636"><rect width="26" height="26" fill="white"/></clipPath></defs></svg>';
-		$itemInfo.prepend($infoIcon);
-	}
-	/* Custom Checkbox */
-	function SenineCustomCheckbox() {
+	/* Checkbox Custom */
+	function SeineCheckboxCustom() {
 		const $itemcheckbox = $('.tnp-privacy-field .tnp-privacy')
 		if (!$itemcheckbox.length) return;
 
 		const $divcheckbox = '<div class="checkmark"></div>';
 		$itemcheckbox.parent().append($divcheckbox);
+		if ($('.bt-newsletter-no-privacy').length > 0) {
+			var privacyCheckbox = $('.bt-newsletter-no-privacy input.tnp-privacy');
+			if (privacyCheckbox.length > 0 && !privacyCheckbox.prop('checked')) {
+				privacyCheckbox.prop('checked', true);
+			}
+		}
 	}
 
 	jQuery(document).ready(function ($) {
@@ -262,8 +260,7 @@
 		SeineBubleEffect();
 		SeineShop();
 		SeineUnitsCustom();
-		SeineInforProduct();
-		SenineCustomCheckbox();
+		SeineCheckboxCustom();
 	});
 
 	jQuery(window).on('resize', function () {
